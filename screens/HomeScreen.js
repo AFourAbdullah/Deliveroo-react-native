7c339ff209350f754dbf6c2f7c46931be8f4cebc
Abdullah Aziz <abdullahazees4@gmail.com>
2023-10-12 20:30:51 +0500

    files added

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,12 +1,39 @@
-import React from "react";
-import { Text, View } from "react-native";
+import { useNavigation } from "@react-navigation/native";
+import React, { useLayoutEffect } from "react";
+import {
+  Image,
+  SafeAreaView,
+  StatusBar,
+  StyleSheet,
+  Text,
+  View,
+} from "react-native";
 
 const HomeScreen = () => {
+  const navigation = useNavigation();
+  useLayoutEffect(() => {
+    navigation.setOptions({
+      headerShown: false,
+    });
+  }, []);
   return (
-    <View>
-      <Text className="text-red-400 bg-white">HomeScreen</Text>
-    </View>
+    <SafeAreaView style={styles.container}>
+      {/* <Text className="text-red-400 bg-white">HomeScreen</Text> */}
+      {/* header */}
+      <View>
+        <Image
+          source={{ uri: "https://links.papareact.com/wru" }}
+          className="h-7 w-7 bg-gray-300 p-4"
+        />
+      </View>
+    </SafeAreaView>
   );
 };
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    paddingTop: StatusBar.currentHeight,
+  },
+});
 
 export default HomeScreen;
