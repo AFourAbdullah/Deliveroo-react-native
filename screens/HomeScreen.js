e206cf49f7873fcd779e6be58abcaf70614f2152
Abdullah Aziz <abdullahazees4@gmail.com>
2023-10-12 20:20:40 +0500

    files added

---
+++ screens/HomeScreen.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { Text, View } from "react-native";
+
+const HomeScreen = () => {
+  return (
+    <View>
+      <Text className="text-red-400 bg-white">HomeScreen</Text>
+    </View>
+  );
+};
+
+export default HomeScreen;
