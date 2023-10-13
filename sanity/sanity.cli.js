8130a8902b9b6e2f0dacf11b30eb4e74b03ee4bc
Abdullah Aziz <abdullahazees4@gmail.com>
2023-10-13 15:50:21 +0500

    files added

---
+++ sanity/sanity.cli.js
@@ -0,0 +1,8 @@
+import {defineCliConfig} from 'sanity/cli'
+
+export default defineCliConfig({
+  api: {
+    projectId: 'o91mdb8o',
+    dataset: 'production'
+  }
+})
---
+++ sanity/schemas/index.js
@@ -0,0 +1,9 @@
+// import blockContent from './blockContent'
+import category from './category'
+import dish from './dish'
+// import post from './post'
+// import author from './dish'
+import featured from './featured'
+import restaurant from './restaurant'
+
+export const schemaTypes = [restaurant, dish, category, featured]
